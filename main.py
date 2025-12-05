89702b69c73cecd757058f584b7f63339af7075e
Bruno David Martins <dbrunobruno369@gmail.com>
2025-12-05 14:30:21 -0300

    Verificando se a entrada está vazia

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from contador import conta_palavras
 
-frase = input("Digite uma frase: ")
-quantidade = conta_palavras(frase)
-print("")
-print(f"A frase contém {quantidade} palavras.")
+frase = input("Digite uma frase: ").strip()
+if not frase:
+    print("Erro: A frase não pode ser vazia.")
+else:
+    resultado = conta_palavras(frase)
+    if resultado:
+        print("Contagem de palavras:")
+        for palavra, quantidade in resultado.items():
+            print(f"{palavra}:{quantidade}")
+    else:
+        print(f"Nenhuma palavra válida foi encontrada.")
