7b55698708df6d8e090bf9cf18f4f4815b6d83fa
Bruno David Martins <dbrunobruno369@gmail.com>
2025-12-05 12:21:59 -0300

    Aprimorando código com criação de funções Aprimorando código com criação de funções

--- contador.py
+++ contador.py
@@ -1,5 +1,8 @@
+def conta_palavras(frase):
+    palavras = frase.split()
+    print(palavras)
+    return len(palavras)
+
 frase = input("Digite uma frase: ")
 
-palavras = frase.split()
-print(len(palavras))
-print(palavras)
+print(conta_palavras(frase))
