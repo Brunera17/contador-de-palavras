379f8f2561415f2496378dd04043a51e46484ff8
Bruno David Martins <dbrunobruno369@gmail.com>
2025-12-05 12:39:37 -0300

    Fazendo o tratamento da frase

--- contador.py
+++ contador.py
@@ -1,4 +1,11 @@
+def limpar_texto(texto):
+    texto = texto.lower()
+    caracteres = ",.!|?;:\"'()[]{}"
+    for char in caracteres:
+        texto = texto.replace(char, "")
+    return texto
+
 def conta_palavras(frase):
+    frase = limpar_texto(frase)
     palavras = frase.split()
-    print(palavras)
     return len(palavras)
